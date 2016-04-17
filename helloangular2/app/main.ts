d6a35d374bbaf4f82439a6b5d4853af37bec5b9c
blackswan <blackswam@blackswan-lenovo>
2016-04-17 20:43:19 +0200

    Chapter 4

--- helloangular2/app/main.ts
+++ helloangular2/app/main.ts
@@ -1,7 +1,7 @@
 /**
  * Created by blackswam on 16/04/16.
  */
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+import { bootstrap }    from 'angular2/platform/browser';
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent);
