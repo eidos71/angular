ac85d2af261d651a2d9f8fb996a9c1f36aee1bac
blackswan <blackswam@blackswan-lenovo>
2016-04-20 13:57:30 +0200

    Moved components

--- todo-angular2/app/app.component.ts
+++ todo-angular2/app/app.component.ts
@@ -1,3 +1,12 @@
 /**
  * Created by blackswam on 18/04/16.
  */
+
+import { Component } from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Test Angular 2 App 2</h1>'
+})
+
+export class AppComponent { }
+
---
+++ todo-angular2/app/boot.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by blackswam on 20/04/16.
+ */
+import {bootstrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
